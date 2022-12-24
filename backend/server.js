02facd1063db086ff072eab44ea8ee8d16fa4a2b
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-24 21:04:09 +0530

    asdf

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ dotenv.config();
 
 // get request
 app.get('/', (req,res) => {
-    res.send("API is running");
+    res.send("API is running Successfully");
 })
 
-app.get('/api/chats', (req,res) => {
+app.get('/api/chat', (req,res) => {
     res.send(chats);
 })
 
-app.get('/api/chats/:id', (req,res) => {
+app.get('/api/chat/:id', (req,res) => {
     // console.log(req.params.id);
     const singleChat = chats.find(c => c._id === req.params.id);
     res.send(singleChat);
