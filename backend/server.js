893f26671f59d89bab911f4d3a8ab753c166f5f1
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-24 20:17:12 +0530

    asdf

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const chats = require("./data/data");
 
-const app = express();
+const app= express();
+
+// get request
+app.get('/', (req,res) => {
+    res.send("API is running");
+})
+
+app.get('/api/chats', (req,res) => {
+    res.send(chats);
+})
+
+app.get('/api/chats/:id', (req,res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find(c => c._id === req.params.id);
+    res.send(singleChat);
+})
 
 app.listen(5000, console.log("Server is running on 5000"));
