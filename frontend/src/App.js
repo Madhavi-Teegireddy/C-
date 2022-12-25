738c580f6f17b40222614a5e86f734a3744f03fb
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-25 17:02:27 +0530

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import { Route } from 'react-router-dom';
 import './App.css';
+import MainRoutes from './Pages/MainRoutes';
 
 function App() {
   return (
     <div className="App">
-      Hello
+       <MainRoutes/>
     </div>
   );
 }
