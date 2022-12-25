551c81bbd775641355478798aac7f8b1c2bb5a2e
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-25 22:56:38 +0530

    frntnd connected with backnd

--- frontend/src/Pages/Chat.jsx
+++ frontend/src/Pages/Chat.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+import axios from "axios";
+
 
 const Chat = () => {
+    const [chats, setChats] = useState([]);
+
+
+    const fetchChats = async () => {
+        const {data} = await axios.get("/api/chat");
+
+        setChats(data);
+    };
+
+    useEffect(() => {
+        fetchChats();
+    },[]);
+
   return (
     <div>
-      My chats
+        { 
+        chats.map((chat =>
+        <div key={chat.id}>{chat.chatName}</div>))}
     </div>
   )
 }
