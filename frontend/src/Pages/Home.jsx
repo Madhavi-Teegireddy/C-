bf7ffbde31af2004075f57f6e11233254da668e4
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-26 23:30:03 +0530

    style

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Container, Box, Tab, TabList, TabPanel, TabPanels, Tabs, Text,  } from "@chakra-ui/react";
+
+
 
 const Home = () => {
   return (
-    <div>My Home</div>
+    <Container maxW="xl" centerContent>
+      <Box
+        d='flex'
+        justifyContent="center"
+        p={3}
+        bg="#90e0ef"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded" colorScheme={"blue"}>
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              {/* <Login /> */}
+            </TabPanel>
+            <TabPanel>
+              {/* <Signup /> */}
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }
 
