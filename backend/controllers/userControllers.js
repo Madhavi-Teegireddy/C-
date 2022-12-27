89c4ee6abde2598a92a2988cb8f48f20fa49a8aa
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-27 20:35:49 +0530

    atl

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,3 +1,12 @@
-const registerUser = () => {
-    
-}
+
+const asyncHandler = require("express-async-handler");
+
+
+const registerUser = asyncHandler(async () => {
+    const {name, email, password, pic} = req.body
+
+    if(!name || !email || !password){
+        resizeBy.status(400);
+        throw new Error("Please Enter all the details");
+    }
+})
--- backend/model/UserModel.jsx
+++ backend/model/UserModel.jsx
@@ -6,7 +6,6 @@ const UserModel = mongoose.Schema({
     password: { type: String, req: true},
     pic: {
         type: String,
-        required: true,
         default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jp"
     },
 }, {
