67d6efbb512e61c8255192fb38bf9c6df7192bd1
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-27 20:07:56 +0530

    mongo

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,12 +5,11 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: true,
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error: ${error.message}`.red.bold);
+    console.log(`Error: ${error.message}`);
     process.exit();
   }
 };
