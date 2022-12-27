06d4e08ae66113d95a71e8a439d0d891d446b59d
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-27 20:13:19 +0530

    mongo

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const colors = require("colors");
+
+
 
 const app= express();
 dotenv.config();
@@ -26,4 +29,4 @@ app.get('/api/chat/:id', (req,res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`Server is running on ${PORT}`));
+app.listen(5000, console.log(`Server is running on ${PORT}`.yellow.bold));
