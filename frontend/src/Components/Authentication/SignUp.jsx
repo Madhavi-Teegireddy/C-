0d18aa28f5e225b043a260d5aea6ff1bca925d59
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-27 10:43:47 +0530

    sign

--- frontend/src/Components/Authentication/SignUp.jsx
+++ frontend/src/Components/Authentication/SignUp.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { FormControl, FormLabel, Input, VStack } from '@chakra-ui/react'
+import { useState } from 'react'
+
+
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [confirmpassword, setConfirmpassword] = useState();
+  const [password, setPassword] = useState();
+  const [pic, setPic] = useState();
+  const [picLoading, setPicLoading] = useState(false);
+    return (
+    <VStack spacing={"5px"}>
+      <FormControl>
+        <FormLabel>
+          <Input
+          placeholder='Enter Your Name'
+          onChange={(e) =>setName(e.target.value)}/>
+        </FormLabel>
+      </FormControl>
+    </VStack>
   )
 }
 
