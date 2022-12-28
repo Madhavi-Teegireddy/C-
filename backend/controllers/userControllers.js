e45bba172f6a834ea8cd4fd51346a7ce6e9136ce
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-28 12:24:00 +0530

    error fixed

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,13 +1,15 @@
 
 const asyncHandler = require("express-async-handler");
-const User = require("../model/UserModel");
+const User = require("../model/userModel");
+const generateToken = require('../config/generateToken');
+
 
 
 const registerUser = asyncHandler(async (req,res) => {
     const {name, email, password, pic} = req.body
 
     if(!name || !email || !password){
-        resizeBy.status(400);
+        res.status(400);
         throw new Error("Please Enter all the details");
     }
 
@@ -31,6 +33,7 @@ const registerUser = asyncHandler(async (req,res) => {
             name: user.name,
             email: user.email,
             pic: user.pic,
+            token:generateToken(user._id),
         });
     } else{
         res.status(400);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,6 @@ const router = express.Router()
 
 
 router.route('/').post(registerUser)
-// router.route('/login',authUser)
+// router.post('/login',authUser)
 
 module.exports = router;
