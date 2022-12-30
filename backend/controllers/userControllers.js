a579ff8f848b43be841c78f400a937f3b79bfb56
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-30 12:39:33 +0530

    allusers route

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -61,8 +61,12 @@ const authUser = asyncHandler(async(req,res) => {
 })
 
 const allUsers = asyncHandler(async(req,res)=>{
-    const keyword = req.query
-    console.log(keyword)
-})
+    const keyword = req.query.search? {
+        $or:[
+        {name: { $regex: req.query.search, $options: "i"}},
+        {email: { $regex: req.query.search, $options: "i"}},
+        ],
+    } : {}
+});
 
 module.exports = { registerUser, authUser, allUsers }
