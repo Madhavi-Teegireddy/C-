3d2c5f120510a72631a2844ac0950364ae626459
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-30 17:21:25 +0530

    allusers route

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const {
-  accessChat
+  accessChat, fetchChats
 } = require("../controllers/chatControllers");
 const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route("/").get(protect, fetchChats);
+router.route("/").get(protect, fetchChats);
 // router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
