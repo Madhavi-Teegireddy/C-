76eecfcb629435589da6f4e5c121039294a23a9a
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-12-30 14:29:33 +0530

    allusers route

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const {
-  accessChat,
-  fetchChats,
-  createGroupChat,
-  removeFromGroup,
-  addToGroup,
-  renameGroup,
+  accessChat
 } = require("../controllers/chatControllers");
-const { protect } = require("../middleware/authMiddleware");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-router.route("/").get(protect, fetchChats);
-router.route("/group").post(protect, createGroupChat);
-router.route("/rename").put(protect, renameGroup);
-router.route("/groupremove").put(protect, removeFromGroup);
-router.route("/groupadd").put(protect, addToGroup);
+// router.route("/").get(protect, fetchChats);
+// router.route("/group").post(protect, createGroupChat);
+// router.route("/rename").put(protect, renameGroup);
+// router.route("/groupremove").put(protect, removeFromGroup);
+// router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
