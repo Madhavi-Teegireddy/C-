b8b461e32ee54c4b2dae91369e9211e63fbffdd8
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-01-09 20:27:28 +0530

    single chat

--- frontend/src/Components/SingleChat.js
+++ frontend/src/Components/SingleChat.js
@@ -12,9 +12,9 @@ import ScrollableChat from "./ScrollableChat";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
-import io from "socket.io-client";
-import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
-import { ChatState } from "../Context/ChatProvider";
+// import io from "socket.io-client";
+// import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
+// import { ChatState } from "../Context/ChatProvider";
 const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
