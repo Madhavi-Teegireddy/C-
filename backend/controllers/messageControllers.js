3c7e6f15863f765d87bee097e3a26cced996de3e
madhavi-teegireddy <madhavitigireddi@gmail.com>
2023-01-10 12:55:07 +0530

    baackend single message api

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -3,7 +3,12 @@ const asyncHandler = require("express-async-handler");
 
 
 const sendMessage = asyncHandler(async(req,res) => {
+  const { content, chatId } = req.body;
 
+  if (!content || !chatId) {
+    console.log("Invalid data passed into request");
+    return res.sendStatus(400);
+  }
 })
 
 module.exports = {
